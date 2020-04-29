da3efc3acd42cf5c11f8ca94cf34fbe6ddd8e3fb
Flavio Campos MM <flaviorcampos15@gmail.com>
2020-04-29 19:09:19 -0300

    Inciando o projeto Room modulo 2 - Aula 01 - Atividade 07

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
@@ -0,0 +1,57 @@
+package br.com.alura.agenda.database;
+
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.room.migration.Migration;
+import androidx.annotation.NonNull;
+
+class AgendaMigrations {
+
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE Aluno ADD COLUMN sobrenome TEXT");
+        }
+    };
+    private static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // Criar nova tabela com as informações desejadas
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` " +
+                    "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`nome` TEXT, " +
+                    "`telefone` TEXT, " +
+                    "`email` TEXT)");
+
+            // Copiar dados da tabela antiga para a nova
+            database.execSQL("INSERT INTO Aluno_novo (id, nome, telefone, email) " +
+                    "SELECT id, nome, telefone, email FROM Aluno");
+
+            // Remove tabela antiga
+            database.execSQL("DROP TABLE Aluno");
+
+            // Renomear a tabela nova com o nome da tabela antiga
+            database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
+        }
+    };
+    private static final Migration MIGRATION_3_4 = new Migration(3, 4) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE Aluno ADD COLUMN momentoDeCadastro INTEGER");
+        }
+    };
+    private static final Migration MIGRATION_4_5 = new Migration(4, 5) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `nome` TEXT, `telefoneFixo` TEXT, `telefoneCelular` TEXT, `email` TEXT, `momentoDeCadastro` INTEGER)");
+
+            database.execSQL("INSERT INTO Aluno_novo (id, nome, telefoneFixo, email, momentoDeCadastro) " +
+                    "SELECT id, nome, telefone, email, momentoDeCadastro FROM Aluno");
+
+            database.execSQL("DROP TABLE Aluno");
+            database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
+
+        }
+    };
+    static final Migration[] TODAS_MIGRATIONS = {MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5};
+
+}
---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,82 @@
+package br.com.alura.agenda.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+@Entity
+public class Aluno implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id = 0;
+    private String nome;
+    private String telefoneFixo;
+    private String telefoneCelular;
+    private String email;
+    private Calendar momentoDeCadastro = Calendar.getInstance();
+
+    public Calendar getMomentoDeCadastro() {
+        return momentoDeCadastro;
+    }
+
+    public void setMomentoDeCadastro(Calendar momentoDeCadastro) {
+        this.momentoDeCadastro = momentoDeCadastro;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefoneFixo() {
+
+        return telefoneFixo;
+    }
+
+    public void setTelefoneFixo(String telefoneFixo) {
+        this.telefoneFixo = telefoneFixo;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return nome + " - " + telefoneFixo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean temIdValido() {
+        return id > 0;
+    }
+
+}
